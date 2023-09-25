25289b73ae34c17fc561432583fd1f055cc6fcd3
luisc5martins <luisc.martinsjunior5@gmail.com>
2023-09-25 11:07:59 -0300

    first-commit

---
+++ src/views/MoviesView.vue
@@ -0,0 +1,85 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import api from '@/plugins/axios'
+
+const movies = ref([]);
+const genres = ref([])
+
+const listMovies = async (genreId) => {
+    const response = await api.get('discover/movie', {
+        params: {
+            with_genres: genreId,
+            language: 'pt-BR'
+        }
+    });
+    movies.value = response.data.results
+};
+
+onMounted(async () => {
+  const response = await api.get('genre/movie/list?language=pt-BR')
+  genres.value = response.data.genres
+})
+</script>
+
+<template>
+    <h1>Filmes</h1>
+    <ul class="genre-list">
+        <li class="genre-item" v-for="genre in genres" :key="genre.id" @click="listMovies(genre.id)">
+            {{ genres.name }}
+        </li>
+    </ul>
+    <div class="movie-list">
+  <div v-for="movie in movies" :key="movie.id" class="movie-card">
+    
+    <img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" :alt="movie.title" />
+    <div class="movie-details">
+      <p class="movie-title">{{ movie.title }}</p>
+      <p class="movie-release-date">{{ movie.release_date }}</p>
+      <p class="movie-genres">{{ movie.genre_ids }}</p>
+    </div>
+    
+  </div>
+</div>
+  </template>
+
+<style scoped>
+.movie-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+
+.movie-card {
+  width: 15rem;
+  height: 30rem;
+  border-radius: 0.5rem;
+  overflow: hidden;
+  box-shadow: 0 0 0.5rem #000;
+}
+
+.movie-card img {
+  width: 100%;
+  height: 20rem;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 0.5rem #000;
+}
+
+.movie-details {
+  padding: 0 0.5rem;
+}
+
+.movie-title {
+  font-size: 1.1rem;
+  font-weight: bold;
+  line-height: 1.3rem;
+  height: 3.2rem;
+}
+.genre-list {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 2rem;
+  list-style: none;
+  margin-bottom: 2rem;
+}
+</style>
